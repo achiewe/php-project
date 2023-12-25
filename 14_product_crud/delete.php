4c3115fccccca8613e3830ca8ecda21db5c1fe30
achiewe <mamukashviliachi706@gmail.com>
2023-12-25 20:45:30 +0400

    add post method insted of get method

--- 14_product_crud/delete.php
+++ 14_product_crud/delete.php
@@ -3,7 +3,7 @@ $pdo = new PDO("mysql:host=localhost;port=3306;dbname=products_crud", "root", ""
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
  
 
-$id = $_GET["id"] ?? null;
+$id = $_POST["id"] ?? null;
 if(!$id){
     header("Location: index.php");
     exit;
