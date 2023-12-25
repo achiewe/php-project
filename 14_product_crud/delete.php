856faa9fa442a70e6fffb8dd8976b6845ce587c3
achiewe <mamukashviliachi706@gmail.com>
2023-12-25 20:20:52 +0400

    add delete function in on the item

--- 14_product_crud/delete.php
+++ 14_product_crud/delete.php
@@ -3,11 +3,16 @@ $pdo = new PDO("mysql:host=localhost;port=3306;dbname=products_crud", "root", ""
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
  
 
-$statement = $pdo->prepare("SELECT * FROM products ORDER BY create_date DESC");
+$id = $_GET["id"] ?? null;
+if(!$id){
+    header("Location: index.php");
+    exit;
+}
+
+
+$statement = $pdo->prepare("DELETE FROM products WHERE id = :id");
+$statement->bindValue(":id", $id );
 $statement->execute();
 
+header("Location: index.php");
 
-
-echo "<pre>";
-var_dump($_GET);
-echo "</pre>";
