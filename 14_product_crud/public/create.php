8821fda29b527373c0756a4a1016ffa66375a134
achiewe <mamukashviliachi706@gmail.com>
2023-12-27 16:52:58 +0400

    create product variable and assing it array to have image key for use it in the form logic

--- 14_product_crud/public/create.php
+++ 14_product_crud/public/create.php
@@ -90,24 +90,12 @@ if($_SERVER['REQUEST_METHOD'] === "POST"){
     </div>
     <?php endif; ?>
 
-    <form method="post" enctype="multipart/form-data">
-  <div class="form-group">
-    <label >product Image</label><br>
-    <input type="file" name="image">
-  </div>
-  <div class="form-group">
-    <label >product title</label>
-    <input type="text" name="title" value="<?php echo $title ?>" class="form-control">
-  </div>
-  <div class="form-group">
-    <label >product decrtiption</label>
-    <textarea class="form-control" name="description"><?php echo $description ?></textarea>
-  </div>
-  <div class="form-group">
-    <label >product price</label>
-    <input type="number" name="price" class="form-control" value="<?php echo $price ?>">
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+<?php 
+  $product = [
+      'image' => ""
+  ];
+?>
+
+   <?php require_once "../views/products/form.php" ?>
   </body>
 </html>
