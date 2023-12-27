53601eb5cc64384ec98e0ce721357296fd2ec8c9
achiewe <mamukashviliachi706@gmail.com>
2023-12-27 12:44:07 +0400

    add the new folder view and partials where i create header.php which i use in the create.php index.php. and update.php

--- 14_product_crud/index.php
+++ 14_product_crud/index.php
@@ -11,20 +11,8 @@ $products = $statement->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
 
+ <?php require_once "views/partials/header.php"; ?>
 
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-     <link href="./app.css" rel="stylesheet"/>
-    <title>product crud</title>
-  </head>
-  <body>
     <h1>product crud</h1>
 
     <p>  
--- 14_product_crud/update.php
+++ 14_product_crud/update.php
@@ -95,19 +95,7 @@ echo "<pre>";
 
 
 
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-     <link href="./app.css" rel="stylesheet"/>
-    <title> new product</title>
-  </head>
-  <body>
+<?php require_once "views/partials/header.php"; ?>
 
   <p>
   <a href="index.php" class="btn btn-secondary"> Back to products</a>
