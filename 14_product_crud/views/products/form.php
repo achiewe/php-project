a4563b4ef8d681f5ab2c6d826e229cdca4a8e1ee
achiewe <mamukashviliachi706@gmail.com>
2023-12-27 16:48:16 +0400

    add inside of the form.php form code

--- 14_product_crud/views/products/form.php
+++ 14_product_crud/views/products/form.php
@@ -1,1 +1,23 @@
-<?php 
+
+<form method="post" enctype="multipart/form-data">
+<?php if($product["image"]): ?>
+    <img src="<?php echo $product["image"] ?>"/>
+    <?php endif; ?>
+<div class="form-group">
+<label >product Image</label><br>
+<input type="file" name="image">
+</div>
+<div class="form-group">
+<label >product title</label>
+<input type="text" name="title" value="<?php echo $title ?>" class="form-control">
+</div>
+<div class="form-group">
+<label >product decrtiption</label>
+<textarea class="form-control" name="description"><?php echo $description ?></textarea>
+</div>
+<div class="form-group">
+<label >product price</label>
+<input type="number" name="price" class="form-control" value="<?php echo $price ?>">
+</div>
+<button type="submit" class="btn btn-primary">Submit</button>
+</form>
